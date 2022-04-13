e6f1415e53f2b707e9a21bf4c1258252d5037edd
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-04-13 11:46:52 +0430

    add header.php

---
+++ include/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.rtl.min.css">
+    
+    <style>
+        @font-face {
+    font-family: 'shabnam';
+
+    src: url('./font/shabnam.ttf') ;
+    
+  }
+
+  .persian{
+      font-family: shabnam;
+  }
+    </style>
+</head>
+<body>
+    
