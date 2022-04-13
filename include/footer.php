a50fc533df084bf4d0aa91b8221be6b2cddfbdd2
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-04-13 16:41:09 +0430

    change footer and header

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,49 @@
+<hr>
+<div class="container-fluid p-5 persian">
+  <footer class="py-5">
+    <div class="row">
+      <div class="col-3">
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">خانه</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">ویژگی ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">طرح ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">پرسش و پاسخ ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">درباره  ما</a></li>
+        </ul>
+      </div>
+      <div class="col-3">
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">خانه</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">ویژگی ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">طرح ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">پرسش و پاسخ ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">درباره  ما</a></li>
+        </ul>
+      </div>
+      <div class="col-3">
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">خانه</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">ویژگی ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">طرح ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">پرسش و پاسخ ها</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">درباره  ما</a></li>
+        </ul>
+      </div>
+
+ 
+
+    <div class="d-flex justify-content-between py-4 my-4 border-top">
+      <p>&copy; 2021</p>
+      <ul class="list-unstyled d-flex">
+        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+
+
 <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
