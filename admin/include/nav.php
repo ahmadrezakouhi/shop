964e84faf1833c20a0b374742c80ceb35576d1d3
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-04-14 18:54:47 +0430

    create admin/include/nav.php

---
+++ admin/include/nav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow p-3">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Admin Shop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persian" href="#" id="goods" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            کالا
+          </a>
+          <ul class="dropdown-menu persian" aria-labelledby="goods">
+            <li><a class="dropdown-item" href="#">کالاها</a></li>
+            <li><a class="dropdown-item" href="../products/create.php">کالای جدید</a></li>
+            
+          </ul>
+        </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persian" href="#" id="category" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            دسته بندی
+          </a>
+          <ul class="dropdown-menu persian" aria-labelledby="category">
+            <li><a class="dropdown-item" href="#">دسته بندی ها</a></li>
+            <li><a class="dropdown-item" href="../categories/create.php">دسته بندی جدید</a></li>
+            
+          </ul>
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link  persian" aria-current="page" href="#">سفارش ها</a>
+        </li>
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
