c0c13fb5dfd63e6156cfaf25941ffbc3bfa2d284
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2022-04-15 17:17:13 +0430

    add categories/index.php

---
+++ admin/categories/index.php
@@ -0,0 +1,35 @@
+<?php 
+include '../include/header.php';
+include '../include/nav.php';
+
+?>
+
+<div class="container d-flex justify-content-center">
+    <div class="table-responsive">
+    <table class="table table-light table-striped table-bordered persian mt-5 shadow text-center" style="width:500px">
+    <thead>
+        <tr>
+            <th>
+                نام
+            </th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>زنانه</td>
+            <td><a class="btn btn-danger persian">حذف</a></td>
+        </tr>
+    </tbody>
+</table>
+    </div>
+</div>
+
+
+
+
+<?php 
+
+include '../include/footer.php';
+
+?>
